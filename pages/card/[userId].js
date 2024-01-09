069864b47566036dbef2aabc0f569065f358fe60
sniffy123 <darwinfacts@gmail.com>
2024-01-09 18:08:51 +0800

    change apiUrl

--- pages/card/[userId].js
+++ pages/card/[userId].js
@@ -11,8 +11,8 @@ const CardPage = () => {
   useEffect(() => {
     const fetchPersonData = async () => {
       try {
-        const fetchAPI = `http://localhost:1337/api/users/${userId}?populate=picture`;
-        // const fetchAPI = `https://forestlake-markers-production.up.railway.app/api/users/${userId}?populate=picture`;
+        // const fetchAPI = `http://localhost:1337/api/users/${userId}?populate=picture`;
+        const fetchAPI = `https://forestlake-markers-production.up.railway.app/api/users/${userId}?populate=picture`;
         const response = await fetch(fetchAPI);
         const data = await response.json();
         console.log(data);
